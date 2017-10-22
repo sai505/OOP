9dcc49a15ba5674d8d23fc35b7613026b73632a8
sai505 <seungjun88@gmail.com>
2017-10-22 17:06:30 -0400

    a

--- card.hpp
+++ card.hpp
@@ -1,1 +1,17 @@
+#pragma once
 
+namespace card {
+	enum Suit { Clubs, Diamonds, Hearts, Spades };
+	enum Rank {
+		Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+		Jack, Queen, King
+	};
+
+	enum Color {Black,Red};
+
+	class Card {
+
+	};
+	
+	class 
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
+//Seung Jun Lee. OOP Class Practice 
+//1.Rewrite your card class into a class hierarchy. 
+//2.Create a class that represents a deck of cards,
+//3.initialize it to a standard deck + 2 jokers, 
+//4.shuffle it
+#include "card.hpp"
+#include <iostream>
 
+int main() {
+
+}
