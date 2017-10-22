91e6aee211c4ed48cad506b86be6485d799fa2be
sai505 <seungjun88@gmail.com>
2017-10-22 18:20:49 -0400

    a

--- card.cpp
+++ card.cpp
@@ -1,7 +1,30 @@
-#inlclude "card.hpp"
+#include "card.hpp"
 
-Gambling::SuitedCard(suit = card::Clubs, rank = card::Ace, Color = card::Black) {
+SuitedCard::SuitedCard() {
+	c_vector.reserve(52);
+	c_vector = {
+		{ Ace, Spades,Black },{ Two, Spades,Black },{ Three, Spades,Black },{ Four, Spades,Black },{ Five, Spades,Black },
+		{ Six, Spades,Black },{ Seven, Spades,Black },{ Eight, Spades,Black },{ Nine, Spades,Black },{ Ten, Spades,Black },
+		{ Jack, Spades,Black },{ Queen, Spades,Black },{ King, Spades,Black },
 
+		{ Ace, Clubs,Black },{ Two, Clubs,Black },{ Three, Clubs,Black },{ Four, Clubs,Black },{ Five, Clubs,Black },
+		{ Six, Clubs,Black },{ Seven, Clubs,Black },{ Eight, Clubs,Black },{ Nine, Clubs,Black },{ Ten, Clubs,Black },
+		{ Jack, Clubs,Black },{ Queen, Clubs,Black },{ King, Clubs,Black },
+
+		{ Ace, Hearts,Red },{ Two, Hearts,Red },{ Three, Hearts,Red },{ Four, Hearts,Red },{ Five, Hearts,Red },
+		{ Six, Hearts,Red },{ Seven, Hearts,Red },{ Eight, Hearts,Red },{ Nine, Hearts,Red },{ Ten, Hearts,Red },
+		{ Jack, Hearts,Red },{ Queen, Hearts,Red },{ King, Hearts,Red },
+
+		{ Ace, Diamonds,Red },{ Two, Diamonds,Red },{ Three, Diamonds,Red },{ Four, Diamonds,Red },{ Five, Diamonds,Red },
+		{ Six, Diamonds,Red },{ Seven, Diamonds,Red },{ Eight, Diamonds,Red },{ Nine, Diamonds,Red },{ Ten, Diamonds,Red },
+		{ Jack, Diamonds,Red },{ Queen, Diamonds,Red },{ King, Diamonds,Red },
+
+		{ None, Joker, Black },{ None, Joker, Red } };
+}
+
+SuitedCard::SuitedCard(Suit s = Clubs, Rank r = Ace, Color c = Black) {
+	assert (c_vector.size == 52) {
+		std::cout << "full deck" << std::endl;
 
 }
 
--- card.hpp
+++ card.hpp
@@ -1,24 +1,34 @@
 #pragma once
-namespace card {
-	enum Suit { Clubs=1, Diamonds, Hearts, Spades };
-	enum Rank {
-		Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
-		Jack, Queen, King};
-	enum Color { Black=1, Red };
-}
-namespace Gambling {
-	class Gambling {
+#include <vector>
+#include <iostream>
+#include<assert.h>
+enum Rank {
+	None,Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+	Jack, Queen, King
+};
+enum Suit { Clubs=1, Diamonds, Hearts, Spades, Joker };
+enum Color { Black=1, Red };
 
-	};
+struct Card_Data {
+	Rank rank;
+	Suit suit;
+	Color color;
+};
+
+class Gambling {
+
+};
 	
-	class SuitedCard:Gambling {
+class SuitedCard:Gambling {
 		
-	public:
-		SuitedCard(suit = card::Clubs, rank=card::Ace, Color=card::Black) {}
-	private:
-		card::Suit suit;
-		card::Rank rank;
-		card::Color color;
+public:
+	SuitedCard();
+	SuitedCard(Suit s = Clubs, Rank r = Ace, Color c = Black) {};	
+	
+private:
+	Suit suit;
+	Rank rank;
+	Color color;
+	std::vector<Card_Data> c_vector;
 
-	};
-}
+};
